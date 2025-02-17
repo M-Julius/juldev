d73f94bc0472627deb0f49b0d08091ddef6524ce
M-Julius <mjulius.dev@gmail.com>
2025-02-17 22:28:18 +0700

    add space at text about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -81,7 +81,7 @@ export default function About() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.4 }}
-                className="text-xl text-primary"
+                className="text-xl text-primary pt-10"
               >
                 ...I have over 5 years of experience in mobile development,
                 specializing in React Native and also have experience in Flutter
